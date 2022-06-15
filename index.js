38883e066f850d232baa5c73a68ddaa9a15078af
TigranZakharyan <zakharyantigran27@gmail.com>
2022-06-15 15:39:04 +0400

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+class MyModule{
+  static toFloat(num){
+    return num.toFixed(1)
+  }
+  static toString(num){
+    return String(num)
+  }
+  static toBool(num){
+    return Boolean(num)
+  }
+} 
